7587ba2ecb6ed269fe65916626e604097f56055c
Irfan Dayan <engr.irfandayan@gmail.com>
2023-01-22 12:09:21 +0500

    Add initials for full stack ecommerce ecomer app

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+// custom components/pages
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemsDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:item" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="/success" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
