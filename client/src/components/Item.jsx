016e52569607ef35f8c243509d8bfd7e9c84be94
Irfan Dayan <engr.irfandayan@gmail.com>
2023-01-29 10:47:07 +0500

    fix typo

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -38,7 +38,6 @@ const Item = ({ item, width }) => {
           alt={item.name}
           width="300px"
           height="400px"
-          // src={`http://localhost:1337${url}`}
           src={`https://strapi-l6as.onrender.com${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
           style={{
